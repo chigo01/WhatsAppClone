05b564b779613feeb0b184efa17a46d8e434ee3d
chigo01 <baruchfavour01@gmail.com>
2024-08-08 18:02:55 +0100

    whatsapp clone swift

---
+++ WhatsAppClone/UpdateTabViewScreen.swift
@@ -0,0 +1,18 @@
+//
+//  UpdateTabView.swift
+//  WhatsAppClone
+//
+//  Created by Mac on 8/8/24.
+//
+
+import SwiftUI
+
+struct UpdateTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpdateTabView()
+}
