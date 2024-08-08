f5eee2d6bfc4280092ea8a5d1afd4e40aedb083a
Favour Baruch <78267322+chigo01@users.noreply.github.com>
2024-08-08 15:56:49 +0100

    whatsapp_swift_app

---
+++ WhatsAppClone/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  WhatsAppClone
+//
+//  Created by Mac on 8/8/24.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
