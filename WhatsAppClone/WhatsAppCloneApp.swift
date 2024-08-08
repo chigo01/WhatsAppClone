fd52645f3b95a439aabe0e73053686b823278a5c
Favour Baruch <78267322+chigo01@users.noreply.github.com>
2024-08-08 14:59:19 +0100

    Initial Commit

---
+++ WhatsAppClone/WhatsAppCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsAppCloneApp.swift
+//  WhatsAppClone
+//
+//  Created by Mac on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct WhatsAppCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
