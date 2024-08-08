82372a1e55289fbab8446a9b5a3e8adbe8ec17d6
chigo01 <baruchfavour01@gmail.com>
2024-08-08 18:07:17 +0100

    whatsapp clone

--- WhatsAppClone/MainTabView.swift
+++ WhatsAppClone/MainTabView.swift
@@ -1,15 +1,84 @@
-//
-//  MainTabView.swift
-//  WhatsAppClone
-//
-//  Created by Mac on 8/8/24.
-//
-
 import SwiftUI
 
+
 struct MainTabView: View {
+//    init(){
+//        makeTabBarOpaque()
+//    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+        placeholderItemView("Updates")
+                .tabItem {
+                    Image(systemName: Tab.updates.icon)
+                    Text(Tab.updates.title)
+                }
+           placeholderItemView("Calls")
+                .tabItem {
+                    Image(systemName: Tab.calls.icon)
+                    Text(Tab.calls.title)
+                }
+            placeholderItemView("Communities")
+                .tabItem {
+                    Image(systemName: Tab.communities.icon)
+                    Text(Tab.communities.title)
+                }
+         placeholderItemView("Chats")
+                .tabItem {
+                    Image(systemName: Tab.chats.icon)
+                    Text(Tab.chats.title)
+                }
+            Text("Settings")
+                .tabItem {
+                    Image(systemName: Tab.settings.icon)
+                    Text(Tab.settings.title)
+                }
+            
+            
+        }
+     
+        
+    }
+    
+//    private func makeTabBarOpaque() {
+//           let appearance =  UITabBarAppearance()
+//           appearance.configureWithOpaqueBackground()
+//           UITabBar.appearance().standardAppearance = appearance
+//           UITabBar.appearance().scrollEdgeAppearance = appearance
+//       }
+//       
+  
+}
+
+
+
+extension MainTabView {
+    
+    private func placeholderItemView(_ title:String) -> some View{
+        Text(title)
+           
+    }
+    
+    private enum Tab: String {
+        case updates, calls, communities, chats, settings
+        
+        fileprivate var title: String {
+            return rawValue.capitalized
+        }
+        
+        fileprivate var icon: String {
+            switch self {
+            case .updates:
+                return "circle.dashed.inset.filled"
+            case .calls:
+                return "phone"
+            case .communities:
+                return "person.3"
+            case .chats:
+                return "message"
+            case .settings:
+                return "gear"
+            }
+        }
     }
 }
 
--- WhatsAppClone/UpdateTabViewScreen.swift
+++ WhatsAppClone/UpdateTabViewScreen.swift
@@ -7,12 +7,98 @@
 
 import SwiftUI
 
-struct UpdateTabView: View {
+struct UpdateTabViewScreen: View {
+    @State private var searchText = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List {
+                StatusSectionHeader()
+                StatusSection()
+            }
+            .navigationTitle("Updates")
+            .searchable(text: $searchText)
+        }
     }
 }
 
+
+private struct StatusSectionHeader: View {
+    var body: some View {
+        HStack(alignment: .top){
+            Image(systemName: "circle.dashed")
+                .foregroundStyle(.blue)
+                .imageScale(.large)
+            
+            //create a textSpan or concatinate view
+            
+            (
+                Text("Use Status to share photos, text and videos that disapper in 24 hours. ")
+                
+                + 
+                Text("Status Privacy")
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/).bold()
+            )
+            
+            Image(systemName: "xmark")
+                .foregroundStyle(.gray)
+            
+            
+            
+        }
+    }
+}
+
+
+private struct StatusSection: View {
+    var body: some View {
+        HStack(){
+            Circle()
+                .frame(width: 55, height: 55)
+            VStack(alignment: .leading){
+                Text("My Status")
+                    .font(.callout)
+                    .bold()
+                
+                Text("Add to my status")
+                    .foregroundStyle(.gray)
+                    .font(.system(size: 15))
+                
+             
+            }
+            Spacer()
+            
+            cameraButton()
+            pencilButton()
+        }
+    }
+    
+    private func cameraButton() -> some View{
+        Button {
+            
+        } label:{
+            Image(systemName: "camera.fill")
+                .padding(10)
+                .background(Color(.systemGray5))
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                .bold()
+        }
+    }
+    
+    private func pencilButton() -> some View{
+        Button {
+            
+        } label:{
+            Image(systemName: "pencil")
+                .padding(10)
+                .background(Color(.systemGray5))
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                .bold()
+        }
+    }
+    
+}
+
+
 #Preview {
-    UpdateTabView()
+    UpdateTabViewScreen()
 }
